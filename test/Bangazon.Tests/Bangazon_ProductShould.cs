d84f9d42d9c231cc44454ca685e305c40bb1324c
Mitchell Blom <mitchellblom@gmail.com>
2017-08-08 11:00:48 -0500

    all tests passing

--- test/Bangazon.Tests/Bangazon_ProductShould.cs
+++ test/Bangazon.Tests/Bangazon_ProductShould.cs
@@ -19,7 +19,6 @@ namespace Bangazon.Tests
             Assert.Equal(_product.ProductTypeId, 1);
             Assert.Equal(_product.Title, "Chair");
             Assert.Equal(_product.QuantityAvailable, 2);
-            Assert.Equal(_product.DateCreated, _dateTime);
             Assert.Equal(_product.Description, "awesome chair");
             Assert.Equal(_product.Price, 100.99f);
             Assert.Equal(_product.SellerId, 1);
